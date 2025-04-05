bb846c6720bb6539993a5bbeb498b521149bf050
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-06 00:39:41 +0600

    print string with value

--- input.c
+++ input.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main(){
-    long long  int inputValue ;
-    scanf("%lld", &inputValue);
-    printf("%lld\n",inputValue);
+    // long long  int inputValue ;
+    // scanf("%lld", &inputValue);
+    // printf("%lld\n",inputValue);
+    int num = 100;
+    printf("Number is = %d",num);
+    return 0;
 }
