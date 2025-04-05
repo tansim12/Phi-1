5fa86d47f1b9df9f719ba9a1fec96a0f53842f20
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-05 23:21:00 +0600

    variable

---
+++ test.c
@@ -0,0 +1,50 @@
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     scanf("%d",&a);
+//     printf("a = %d",a);
+//     return 0;
+   
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//   printf("Hello World!");
+//   return 0;
+// }
+
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+
+
+  int myNum = 15;
+  char myLetter = 'D';  
+  char myText[] = "Hello";
+  double myDouble = 5.99;
+  float myFloat = 4.99;
+  bool myBoolean = false;   
+
+  int x = 10 , y =20, z=30;
+  printf("%d",z);
+
+  printf("%c\n", myLetter);
+  printf("%s\n",myText);
+  myNum = 156;
+  printf("%d\n" ,myNum);
+  printf("%.2lf\n", myDouble); 
+  printf("%.3f\n", myFloat); 
+  printf("%d\n" ,myBoolean);
+  return  0;
+}
+
+
+//  #include <stdio.h>
+// int main()
+// {
+//   printf("hello225\n");
+//   printf("hello225");
+//   return 0;
+// }
