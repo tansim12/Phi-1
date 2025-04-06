3b6dfefab66e5f2f73954a2c21dbf2e7fedfb87b
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-06 21:55:31 +0600

    dynamic input and else if

---
+++ oparator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int y;
+
+    scanf("%d  %d", &x, &y);
+
+    if (x > y)
+    {
+        /* code */
+        printf("value of x = %d", x);
+    }
+    else
+    {
+        printf("value of y = %d", y);
+    }
+  
+
+    
+
+    return 0;
+}
