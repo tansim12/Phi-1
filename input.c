d01419234f50410849e6405b40665c2203791362
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-06 21:47:08 +0600

    multiple input

--- input.c
+++ input.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main(){
-    // long long  int inputValue ;
-    // scanf("%lld", &inputValue);
-    // printf("%lld\n",inputValue);
-    int num = 100;
-    printf("Number is = %d",num);
+    long long  int inputValue ;
+    double inputFloat ;
+    char inputChar ;
+    scanf("%lld %lf %c", &inputValue , &inputFloat,&inputChar);
+    printf("%lld\n %lf\n %c\n ",inputValue,inputFloat,inputChar);
+    // int num = 100;
+    // printf("Number is = %d",num);
     return 0;
 }
