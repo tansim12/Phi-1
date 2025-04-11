bed171f382da3cf923d6fb31a3d7f0a4ad141e33
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-11 18:21:01 +0600

    dynamic input and using loop for calculate

--- loop.c
+++ loop.c
@@ -2,21 +2,43 @@
 
 int main()
 {
-    int a = 1;
-    int b = 5;
-    while (a < b)
-    {
-        // printf("%d\n", b);
-        a++; 
-        printf("%d\n", a);
-    }
 
-    for (size_t i = 0; i < 10; ++i)
+    //* normal loop
+    // int a = 1;
+    // int b = 5;
+    // while (a < b)
+    // {
+    //     // printf("%d\n", b);
+    //     a++;
+    //     printf("while =>%d\n", a);
+    // }
+
+    // for (size_t i = 0; i <= 10; ++i)
+    // {
+    //     /* code */
+    //     a++;
+    //     printf("for => %d\n", a);
+    // }
+
+    // do
+    // {
+    //     /* code */
+    //     a++;
+    //     printf("do =>%d\n", a);
+
+    // } while (a>b);
+
+    // * Dynamic loop
+    int num;
+    int total;
+    scanf("%d", &num);
+
+    for (size_t i = 1; i <= num; i++)
     {
         /* code */
-        a++; 
-        printf("for => %d\n", a);
+        total += i;
     }
-    
+    printf("total => %d", total);
+
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+    // do
+    // {
+    //     /* code */
+    //     a++; 
+    //     printf("do =>%d\n", a);
+        
+    // } while (a>b);
