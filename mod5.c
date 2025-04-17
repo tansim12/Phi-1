c8551ab4efbe803dbaa6b25b7f37b3c41a4421da
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-17 15:35:25 +0600

    p1

---
+++ mod5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    scanf("%d %d",&a,&b);
+    if (a>=b)
+    {
+        /* code */
+        printf("Yes");
+    }else{
+        printf("no");
+    }
+    
+    return 0;
+}
