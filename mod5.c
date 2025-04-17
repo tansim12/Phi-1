db88a49eda83dde74ae994d6e8cc86a0671fe22e
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-17 21:02:25 +0600

    m5 p3

--- mod5.c
+++ mod5.c
@@ -18,33 +18,59 @@
 //     return 0;
 // }
 
-// !2
+// // !2
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a;
+//     int b;
+//     int middle;
+//     scanf("%d %d", &a, &b);
+
+//     if (a > b)
+//     {
+//         /* code */
+//         middle = a;
+//         a = b;
+//         b = middle;
+//     }
+//     if (b % a == 0)
+//     {
+//         /* code */
+//         printf("Multiples");
+//     }
+//     else
+//     {
+
+//         printf("No Multiples");
+//     }
+
+//     return 0;
+// }
 
 #include <stdio.h>
 
 int main()
 {
-    int a;
-    int b;
-    int middle;
-    scanf("%d %d", &a, &b);
+    int n;
 
-    if (a > b)
+    scanf("%d", &n);
+    while (n >= 1000)
     {
         /* code */
-        middle = a;
-        a = b;
-        b = middle;
-    }
-    if (b % a == 0)
-    {
-        /* code */
-        printf("Multiples");
-    }
-    else
-    {
-
-        printf("No Multiples");
+        n = n / 1000;
+        if (n % 2 == 0)
+        {
+            /* code */
+            printf("EVEN");
+        }
+        else
+        {
+            /* code */
+            printf("ODD");
+        }
     }
 
     return 0;
