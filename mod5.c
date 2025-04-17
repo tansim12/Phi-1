ce37b60a985dbf2229d23df1340a959c22819e99
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-17 16:06:28 +0600

    m-5 problem 2

--- mod5.c
+++ mod5.c
@@ -1,17 +1,51 @@
+
+//! 1
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a;
+//     int b;
+//     scanf("%d %d",&a,&b);
+//     if (a>=b)
+//     {
+//         /* code */
+//         printf("Yes");
+//     }else{
+//         printf("no");
+//     }
+
+//     return 0;
+// }
+
+// !2
+
 #include <stdio.h>
 
 int main()
 {
     int a;
     int b;
-    scanf("%d %d",&a,&b);
-    if (a>=b)
+    int middle;
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
     {
         /* code */
-        printf("Yes");
-    }else{
-        printf("no");
+        middle = a;
+        a = b;
+        b = middle;
     }
-    
+    if (b % a == 0)
+    {
+        /* code */
+        printf("Multiples");
+    }
+    else
+    {
+
+        printf("No Multiples");
+    }
+
     return 0;
 }
