17a563447bb8d9f0ec53d84c001fbe7cf45f8ff0
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-18 23:44:17 +0600

    max and min number

--- mod5.c
+++ mod5.c
@@ -104,26 +104,50 @@
 // }
 
 // ! capital or small ar big digit
+// #include <stdio.h>
+
+// int main()
+// {
+//   char ch;
+//   scanf("%c", &ch);
+//   if (ch >= 'a' && ch <= 'z')
+//   {
+//     printf("ALPHA\n");
+//     printf("IS SMALL");
+//   }
+//   else if (ch >= 48 && ch <= 57)
+//   {
+//     printf("IS DIGIT");
+//   }
+//   else if (ch >= 'A' && ch <= 'Z')
+//   {
+//     printf("ALPHA\n");
+//     printf("IS CAPITAL");
+//   }
+
+//   return 0;
+// }
+
+// ! max and min number
 #include <stdio.h>
 
 int main()
 {
-  char ch;
-  scanf("%c", &ch);
-  if (ch >= 'a' && ch <= 'z')
-  {
-    printf("ALPHA\n");
-    printf("IS SMALL");
-  }
-  else if (ch >= 48 && ch <= 57)
-  {
-    printf("IS DIGIT");
-  }
-  else if (ch >= 'A' && ch <= 'Z')
-  {
-    printf("ALPHA\n");
-    printf("IS CAPITAL");
-  }
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+  int min = a;
+  int max = a;
+  if (b >= max)
+    max = b;
+  if (c >= max)
+    max = c;
+
+  if (b <= min)
+    min = b;
+  if (c <= min)
+    min = c;
+
+  printf("%d %d\n", min, max);
 
   return 0;
 }
