9076448a49668012a95e96a64ff9646f34acabfa
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-18 22:37:08 +0600

    capital or small ar big digit

--- mod5.c
+++ mod5.c
@@ -50,6 +50,8 @@
 //     return 0;
 // }
 
+//! First ডিজিট
+
 // #include <stdio.h>
 
 // int main()
@@ -76,24 +78,51 @@
 //     return 0;
 // }
 
+// ! n char
+
+// #include <stdio.h>
+
+// int main()
+// {
+//   char X;
+
+//   scanf("%c", &X);
+//   scanf("%d", &X);
+//   int c = X;
+//   if (c >= 65 && c <= 90)
+//   {
+//     c = c + 32;
+//     printf("%c", c);
+//   }
+//   else
+//   {
+//     c = c - 32;
+//     printf("%c", c);
+//   }
+
+//   return 0;
+// }
+
+// ! capital or small ar big digit
 #include <stdio.h>
 
 int main()
 {
-  char X;
-
-  scanf("%c", &X);
-  scanf("%d", &X);
-  int c = X;
-  if (c >= 65 && c <= 90)
+  char ch;
+  scanf("%c", &ch);
+  if (ch >= 'a' && ch <= 'z')
   {
-    c = c + 32;
-    printf("%c", c);
+    printf("ALPHA\n");
+    printf("IS SMALL");
   }
-  else
+  else if (ch >= 48 && ch <= 57)
   {
-    c = c - 32;
-    printf("%c", c);
+    printf("IS DIGIT");
+  }
+  else if (ch >= 'A' && ch <= 'Z')
+  {
+    printf("ALPHA\n");
+    printf("IS CAPITAL");
   }
 
   return 0;
