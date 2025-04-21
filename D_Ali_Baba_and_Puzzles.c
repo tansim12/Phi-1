c638cfd68355b4be5c79b2fab244816edb6a7443
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-21 15:44:10 +0600

    divisors

---
+++ D_Ali_Baba_and_Puzzles.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    long long int d;
+    scanf("%d %d %d %lld", &a, &b, &c, &d);
+    long long int c1 = a + b - c;
+    long long int c2 = a - b + c;
+    long long int c3 = a + b * c;
+    long long int c4 = a * b + c;
+    long long int c5 = a * b - c;
+    long long int c6 = a - b * c;
+
+    if (c1 == d)
+    {
+        printf("YES");
+    }
+    else if (c2 == d)
+    {
+        printf("YES");
+    }else if (c3 == d)
+    {
+        printf("YES");
+    }else if (c4 == d)
+    {
+        printf("YES");
+    }else if (c5 == d)
+    {
+        printf("YES");
+    }else if (c6 == d)
+    {
+        printf("YES");
+    }else {
+        printf("NO");
+    }
+
+    return 0;
+}
