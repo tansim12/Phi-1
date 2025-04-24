08d756c3464844d2bca0c2d4bfd22a686c6c2f92
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-24 19:33:14 +0600

    array swap

--- mod9.c
+++ mod9.c
@@ -56,28 +56,42 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     int index;
+//     scanf("%d", &index);
+//     for (int i = index; i < n-1; i++)
+//     {
+//         arr[i]=arr[i+1];
+//     }
+//     for (int i = 0; i < n-1; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    int index;
-    scanf("%d", &index);
-    for (int i = index; i < n-1; i++)
-    {
-        arr[i]=arr[i+1];
-    }
-    for (int i = 0; i < n-1; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int middle;
+    middle = a;
+    a = b;
+    b = middle;
+    printf("%d %d", a, b);
     return 0;
 }
