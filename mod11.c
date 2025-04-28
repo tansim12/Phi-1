32222b16ab0d027936c8b9baf2b553507868f42a
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-28 21:18:42 +0600

    string concat

--- mod11.c
+++ mod11.c
@@ -37,3 +37,31 @@
 
 //     return 0;
 // }
+
+
+// ! string concat
+// #include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+    
+//     char str1[101],str2[101], str3[201];
+//     scanf("%s %s",&str1 , &str2);
+//     int length1 = strlen(str1);
+//     int length2 = strlen(str2);
+//     for ( int i = 0; i <= length1; i++)
+//     {
+//         /* code */
+//         str3[i]=str1[i];
+//     }
+//     for ( int i = 0; i < length2; i++)
+//     {
+//         /* code */
+//         str3[i+length1]=str2[i];
+//     }
+    
+
+//     printf("%s %s %s",str1 ,str2 ,  str3);
+
+//     return 0;
+// }
