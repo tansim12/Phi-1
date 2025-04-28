acc8c380bd6a930eb598d80547b555dfa8aa33d3
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-28 19:32:25 +0600

    string copy shortcut with strcpy

--- mod11.c
+++ mod11.c
@@ -1,20 +1,39 @@
-#include <stdio.h>
-#include <string.h>
 
-int main()
-{
-    char arr1[101],arr2[101];
-    scanf("%s %s",&arr1 , &arr2);
+// ! string copy 
+// #include <stdio.h>
+// #include <string.h>
 
-    int lengthOrArr2 = strlen(arr2);
+// int main()
+// {
+//     char str1[101],str2[101];
+//     scanf("%s %s",&str1 , &str2);
 
-    for ( int i = 0; i <= lengthOrArr2; i++)
-    {
-        /* code */
-        arr1[i] = arr2[i];
-    }
+//     int lengthOrArr2 = strlen(str2);
+
+//     for ( int i = 0; i <= lengthOrArr2; i++)
+//     {
+//         /* code */
+//         str1[i] = str2[i];
+//     }
     
-    printf("%s %s",arr1 ,arr2);
+//     printf("%s %s",str1 ,str2);
     
-    return 0;
-}
+//     return 0;
+// }
+
+
+// ! string copy shortcut with strcpy 
+// #include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+    
+//     char str1[101],str2[101], str3[101];
+//     scanf("%s %s",&str1 , &str2);
+
+//     strcpy(str3,str2); 
+
+//     printf("%s %s %s",str1 ,str2 ,  str3);
+
+//     return 0;
+// }
