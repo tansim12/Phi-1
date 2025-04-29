052f03ce5083de120b029874e14176bb2d979e22
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-04-29 23:14:45 +0600

    strcat

--- mod11.c
+++ mod11.c
@@ -1,5 +1,5 @@
 
-// ! string copy 
+// ! string copy
 // #include <stdio.h>
 // #include <string.h>
 
@@ -15,36 +15,34 @@
 //         /* code */
 //         str1[i] = str2[i];
 //     }
-    
+
 //     printf("%s %s",str1 ,str2);
-    
+
 //     return 0;
 // }
 
-
-// ! string copy shortcut with strcpy 
+// ! string copy shortcut with strcpy
 // #include <stdio.h>
 // #include <string.h>
 // int main()
 // {
-    
+
 //     char str1[101],str2[101], str3[101];
 //     scanf("%s %s",&str1 , &str2);
 
-//     strcpy(str3,str2); 
+//     strcpy(str3,str2);
 
 //     printf("%s %s %s",str1 ,str2 ,  str3);
 
 //     return 0;
 // }
 
-
 // ! string concat
 // #include <stdio.h>
 // #include <string.h>
 // int main()
 // {
-    
+
 //     char str1[101],str2[101], str3[201];
 //     scanf("%s %s",&str1 , &str2);
 //     int length1 = strlen(str1);
@@ -59,9 +57,22 @@
 //         /* code */
 //         str3[i+length1]=str2[i];
 //     }
-    
 
 //     printf("%s %s %s",str1 ,str2 ,  str3);
 
 //     return 0;
 // }
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+
+    char str1[101], str2[101], str3[201];
+    scanf("%s %s", &str1, &str2);
+    strcat(str1,str2);
+
+    printf("%s %s %s", str1, str2, str3);
+
+    return 0;
+}
