130fe3e1ff28e031ce214fdaed8eb4b09fbfc059
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-05-01 15:04:16 +0600

    sum degits

---
+++ K_Sum_Digits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n ;
+    char str[1000001];
+    scanf("%d %s",&n,&str);
+    int sum =0;
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        sum = sum + str[i]-48;
+
+    }
+    printf("%d",sum);
+    return 0;
+}
