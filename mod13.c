5848abc2da934c144882cfd7b73f5147e71441d2
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-05-04 11:15:51 +0600

    nested loop

---
+++ mod13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int tc ;
+    scanf("%d",&tc);
+    for (int i = 1; i <=tc; i++)
+    {
+        /* code */
+        for (int j = 0; j < i; j++)
+        {
+            /* code */
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
