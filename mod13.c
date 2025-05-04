1f7bf7974ce3e360523eb353e323cd5df37063f9
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-05-04 19:23:42 +0600

    pyramid

--- mod13.c
+++ mod13.c
@@ -65,23 +65,87 @@
 // }
 
 //! reverse 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n ;
+//     scanf("%d",&n);
+//     int star=n;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         /* code */
+//         for (int j = 1; j <= star; j++)
+//         {
+//             /* code */
+//             printf("*");
+//         }
+//         printf("\n");
+//         star--;
+        
+//     }
+    
+//     return 0;
+// }
+
+
+
+// ! pyramid 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n,star=1 ;
+//     scanf("%d",&n);
+//     int space = n-1 ;
+//     for (int i = 1; i <=n; i++) // line print loop 
+//     {
+//         // space print loop 
+//         for (int s = 1; s <=space; s++)
+//         {    
+//             printf(" ");
+//         }
+        
+//         // star print loop 
+//         for (int j = 1; j <= star; j++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//         star+=2;
+//         space--;
+
+        
+//     }
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
 int main()
 {
     int n ;
     scanf("%d",&n);
-    int star=n;
+    int star = 1;
+    int space = n-1;
     for (int i = 1; i <=n; i++)
     {
-        /* code */
-        for (int j = 1; j <= star; j++)
+        for (int s = 1; s <=space; s++)
         {
             /* code */
+            printf(" ");
+        }
+        
+        // star print
+        for (int j = 1; j <=star; j++)
+        {
             printf("*");
         }
         printf("\n");
-        star--;
+        star+=2;
+        space--;
         
     }
     
