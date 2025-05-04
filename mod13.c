45c0cc810c6949cad099aab3e3c83889408d1801
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-05-04 11:24:14 +0600

    star revarse print

--- mod13.c
+++ mod13.c
@@ -1,10 +1,31 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int tc ;
+//     scanf("%d",&tc);
+//     for (int i = 1; i <=tc; i++)
+//     {
+//         /* code */
+//         for (int j = 0; j < i; j++)
+//         {
+//             /* code */
+//             printf("*");
+//         }
+//         printf("\n");
+        
+//     }
+    
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
     int tc ;
     scanf("%d",&tc);
-    for (int i = 1; i <=tc; i++)
+    for (int i = tc; i >=0; i--)
     {
         /* code */
         for (int j = 0; j < i; j++)
