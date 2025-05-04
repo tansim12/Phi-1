eb3f6e543e34b8e47d34973087e3f102a7251cc6
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-05-04 14:42:29 +0600

    reverse star

--- mod13.c
+++ mod13.c
@@ -19,21 +19,69 @@
 //     return 0;
 // }
 
+// ! reverse print 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int tc ;
+//     scanf("%d",&tc);
+//     for (int i = tc; i >=0; i--)
+//     {
+//         /* code */
+//         for (int j = 0; j < i; j++)
+//         {
+//             /* code */
+//             printf("*");
+//         }
+//         printf("\n");
+        
+//     }
+    
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n,star=1 ;
+//     scanf("%d",&n);
+//     for (int i = 1; i <=n; i++)
+//     {
+//         /* code */
+//         for (int j = 1; j <= star; j++)
+//         {
+//             /* code */
+//             printf("*");
+//         }
+//         printf("\n");
+//         star++;
+        
+//     }
+    
+//     return 0;
+// }
+
+//! reverse 
 #include <stdio.h>
 
 int main()
 {
-    int tc ;
-    scanf("%d",&tc);
-    for (int i = tc; i >=0; i--)
+    int n ;
+    scanf("%d",&n);
+    int star=n;
+    for (int i = 1; i <=n; i++)
     {
         /* code */
-        for (int j = 0; j < i; j++)
+        for (int j = 1; j <= star; j++)
         {
             /* code */
             printf("*");
         }
         printf("\n");
+        star--;
         
     }
     
